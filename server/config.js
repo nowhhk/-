8e2e444c05ca4614ee61a99818f2005bf67f58bc
hhk <nowhhk@gmail.com>
2022-10-18 18:05:56 +0900

    feature(server): env, config 설정

---
+++ server/config.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+//환경변수에 value가 없다면 에러를 던져주는 함수
+function required(key, defaultValue = undefined) {
+  const value = process.env[key] || defaultValue;
+  if (value == null) {
+    throw new Error(`key ${key} is undefined`);
+  }
+  return value;
+}
+export const config = {
+  jwt: {
+    secretKey: required('JWT_SECRET'),
+    expiresInSec: parseInt(required('JWT_EXPIRES_SEC', 86400)),
+  },
+  bcrypt: {
+    saltRounds: parseInt(required('BCRUPT_SALT_ROUNDS', 12)),
+  },
+  host: {
+    port: required('HOST_PORT', 8080),
+  },
+};
