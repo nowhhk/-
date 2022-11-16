03fa743ac7be6661d7ae53e36ce1e249961fdaa1
hhk <nowhhk@gmail.com>
2022-11-16 23:45:37 +0900

    refac(server): sequelize 적용

--- server/db/database.js
+++ server/db/database.js
@@ -1,10 +1,10 @@
-import mysql from 'mysql2';
 import { config } from '../config.js';
+import SQ from 'sequelize';
 
-const pool = mysql.createPool({
-  host: config.db.host,
-  user: config.db.user,
-  database: config.db.database,
+const { host, user, database, password } = config.db;
+
+export const sequelize = new SQ.Sequelize(database, user, password, {
+  host,
+  dialect: 'mysql',
+  logging: false,
 });
-
-export const db = pool.promise();
