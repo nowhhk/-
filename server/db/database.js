d79b3ba65a4d6d88aae48c36f427610b6bb6ac79
hhk <nowhhk@gmail.com>
2022-11-17 12:10:56 +0900

    chore: configuration

--- server/db/database.js
+++ server/db/database.js
@@ -5,6 +5,7 @@ const pool = mysql.createPool({
   host: config.db.host,
   user: config.db.user,
   database: config.db.database,
+  password: config.db.password,
 });
 
 export const db = pool.promise();
